3e92b6c473b2c47cadbab7b6b9d59fc1b476fecc
johnnyleekahlong-dev <134179465+johnnyleekahlong-dev@users.noreply.github.com>
2023-09-21 15:09:37 +0800

    created analytics service

---
+++ utils/analytics.generator.ts
@@ -0,0 +1,47 @@
+import { Document, Model } from "mongoose";
+
+interface MonthData {
+  month: string;
+  count: number;
+}
+
+export async function generateLast12MonthsData<T extends Document>(
+  model: Model<T>
+): Promise<{ last12Months: MonthData[] }> {
+  const last12Months: MonthData[] = [];
+  const currentDate = new Date();
+
+  currentDate.setDate(currentDate.getDate() + 1);
+
+  // for 12 months data
+  for (let i = 11; i >= 0; i--) {
+    const endDate = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      currentDate.getDate() - i * 28 // 28 days in a month
+    );
+
+    const startDate = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      currentDate.getDate() - 28
+    );
+
+    const monthYear = endDate.toLocaleString("default", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    });
+
+    const count = await model.countDocuments({
+      createdAt: {
+        $gte: startDate,
+        $lt: endDate,
+      },
+    });
+
+    last12Months.push({ month: monthYear, count });
+  }
+
+  return { last12Months };
+}
